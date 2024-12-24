3bcfeae3679c2b8f4b5f87a07ca1d7b9e679a51f
Daniel Vargas <daniel.rvargas10@gmail.com>
2024-12-23 22:11:02 -0300

    initial commit

---
+++ src/main/java/com/devsuperior/desafio_capitulo_2/DesafioCapitulo2Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio_capitulo_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCapitulo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCapitulo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/desafio_capitulo_2/DesafioCapitulo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio_capitulo_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCapitulo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
